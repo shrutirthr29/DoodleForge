2686465ddbb1069f016d6f416ae733511fa602bf
shrutirthr29 <shray.shruti@gmail.com>
2024-05-31 18:55:16 +0530

    controls part only structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { GiArrowCursor } from "react-icons/gi";
+import { RiRectangleLine } from "react-icons/ri";
+import { FaRegCircle } from "react-icons/fa";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { FaPencilAlt } from "react-icons/fa";
+import { MdFileDownload } from "react-icons/md";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='relative w-full h-screen overflow-hidden'>
+        {/* Controls */}
+        <div className='absolute top-0 z-10 w-full'>
+          <div className='flex justify-center items-center gap-3 py-2 px-3 w-fit mx-auto border shadow-lg rounded-lg'>
+            <button>
+              <GiArrowCursor size={"2rem"} />
+            </button>
+            <button>
+              <RiRectangleLine size={"2rem"} />
+            </button>
+            <button>
+              <FaRegCircle size={"2rem"} />
+            </button>
+            <button>
+              <FaLongArrowAltRight size={"2rem"} />
+            </button>
+            <button>
+              <FaPencilAlt size={"2rem"} />
+            </button>
+            <button>
+              <MdFileDownload size={"2rem"} />
+            </button>
+          </div>
+        </div>
+        {/* Canvas */}
+       
+
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
+
 }
 
 export default App
